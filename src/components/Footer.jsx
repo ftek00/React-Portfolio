dae89f5db487e122569dc610196e1c6f59bae00c
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-11-12 16:29:02 -0500

    created header and footer with links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/ftek00"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/yourusername"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="mailto:felegteklezghi@gmail.com">Email Me</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./Navigation";
+import "../App.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Developer Portfolio</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
